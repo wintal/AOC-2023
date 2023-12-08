74fe396c4d959fdd38d542ba7d3bdb81b1631a0d
Leighton Haynes <leighton.haynes@deswik.com>
2023-12-08 15:01:17 +0800

    Day 8 - bit unimpressed with how 'special' the data is here. Spent ages on a general solution before just trying it with 'assume they've made it easy' to find they had.

--- AOC2023/Day8/Program.cs
+++ AOC2023/Day8/Program.cs
@@ -8,9 +8,9 @@ class Day8
 {
     static int Main()
     {
-        DoPart1("sample.txt");
+      //  DoPart1("sample.txt");
         DoPart1("input.txt");
-        DoPart2("sample.txt");
+     //   DoPart2("sample.txt");
         DoPart2("input.txt");
         return 0;
     }
@@ -18,16 +18,30 @@ class Day8
 
     class Node
     {
-
         public string From;
         public String Right;
         public String Left;
+    }
 
+    private static long GreatestCommonDivisor(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private static long LowestCommonMultiple(long a, long b)
+    {
+        return a * b / GreatestCommonDivisor(a,b); 
     }
 
     static void DoPart1(string inputFile)
     {
-       
+
         DateTime start = DateTime.Now;
         var lines = System.IO.File.ReadAllLines(inputFile);
 
@@ -39,31 +53,26 @@ class Day8
         {
             var line = lines[i];
             var parts = line.Split('=');
-            if (parts.Length != 2) continue;
+            if (parts.Length != 2)
+                continue;
             var node = new Node();
             node.From = parts[0].Trim();
             node.Left = parts[1].Split(",")[0].Trim().Substring(1).Trim();
             node.Right = parts[1].Split(",")[1].Trim().Substring(0).Trim();
-            node.Right = node.Right.Substring(0, node.Right.Length-1);
+            node.Right = node.Right.Substring(0, node.Right.Length - 1);
             nodes[node.From] = node;
         }
         DateTime end = DateTime.Now;
-        
 
+
+
+        string location = "AAA";
         int steps = 0;
-        List<string> locations = nodes.Keys.Where(key => key.EndsWith("A")).ToList();
-        while (!locations.All(key => key.EndsWith("Z"))) 
+        do
         {
-            for (int i = 0; i < locations.Count; i++) {
-                var location = locations[i];
-
-                string newLocation = (directions[steps % directions.Length] == 'L')
-                    ? nodes[location].Left
-                    : nodes[location].Right;
-                locations[i] = newLocation;
-            }
+            location = directions[steps % directions.Length] == 'L' ? nodes[location].Left : nodes[location].Right;
             steps++;
-        }
+        } while (!location.EndsWith("ZZZ"));
         Console.WriteLine($"Part 2 ({inputFile})- {steps}");
 
         Console.WriteLine($"Completed part 2 in {end - start}");
@@ -72,20 +81,50 @@ class Day8
 
     static void DoPart2(string inputFile)
     {
-     
-
         DateTime start = DateTime.Now;
         var lines = System.IO.File.ReadAllLines(inputFile);
 
         long solution = 0;
 
-        for (int i = 0; i < lines.Length; i++)
+        var directions = lines[0].Trim();
+        Dictionary<string, Node> nodes = new Dictionary<string, Node>();
+        for (int i = 1; i < lines.Length; i++)
         {
             var line = lines[i];
+            var parts = line.Split('=');
+            if (parts.Length != 2)
+                continue;
+            var node = new Node();
+            node.From = parts[0].Trim();
+            node.Left = parts[1].Split(",")[0].Trim().Substring(1).Trim();
+            node.Right = parts[1].Split(",")[1].Trim().Substring(0).Trim();
+            node.Right = node.Right.Substring(0, node.Right.Length - 1);
+            nodes[node.From] = node;
+        }
+        DateTime end = DateTime.Now;
+
+
+        List<string> locations = nodes.Keys.Where(key => key.EndsWith("A")).ToList();
+
+        int[] cycles = new int[locations.Count];
+        for (int i = 0; i < locations.Count; i++)
+        {
+            string location = locations[i];
+            int steps = 0;
+            do
+            {
+                location = directions[steps % directions.Length] == 'L' ? nodes[location].Left : nodes[location].Right;
+                steps++;
+            } while (!location.EndsWith("Z"));
+            cycles[i] = steps;
         }
 
-        DateTime end = DateTime.Now;
-        Console.WriteLine($"Part 2 ({inputFile})- {solution}");
+        long totalSteps = cycles[0];
+        for (int i = 1; i < locations.Count; i++)
+        {
+            totalSteps = LowestCommonMultiple(totalSteps, cycles[i]);
+        }
+        Console.WriteLine($"Part 2 ({inputFile})- {totalSteps}");
 
         Console.WriteLine($"Completed part 2 in {end - start}");
     }
